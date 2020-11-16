5ceae42071c46e261d4ebf323be76c90a13ba733
David Vanlaningham <dsvanlani@gmail.com>
2020-11-16 11:38:56 -0500

    Added Procfile

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3bso3zn7rns&fpkzno($j#7f!gm=lq++4zzlkk*sctm_=e!2$v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
